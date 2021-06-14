ace639c88e720e68e239c608064ea5caaa353b95
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-06-14 16:39:38 +0300

    done

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,7 +7,7 @@ export class Map extends Component {
       <div>
         <img
           className="map"
-          src={`https://locationiq.com/docs-html/images-maps/streets.png`}
+          src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.props.cityData.lat},${this.props.cityData.lon}&zoom=15`}
           alt=""
         />
       </div>
